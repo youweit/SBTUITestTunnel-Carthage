8a151eab470fe1bbe9890dfa7f08bc54c51d1a9d
Youwei Teng <teng@dcard.cc>
2022-12-06 01:42:31 +0800

    Initial Commit

---
+++ SBTUITestTunnel/SBTUITestTunnel.h
@@ -0,0 +1,18 @@
+//
+//  SBTUITestTunnel.h
+//  SBTUITestTunnel
+//
+//  Created by Youwei Teng on 2022/12/6.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SBTUITestTunnel.
+FOUNDATION_EXPORT double SBTUITestTunnelVersionNumber;
+
+//! Project version string for SBTUITestTunnel.
+FOUNDATION_EXPORT const unsigned char SBTUITestTunnelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBTUITestTunnel/PublicHeader.h>
+
+
